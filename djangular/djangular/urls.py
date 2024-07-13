3c630ebaf45907a66c66ae042528f0ba1cd31805
Julio Chura <jchuraaca@unsa.edu.pe>
2024-07-13 13:54:26 -0500

    Se copia el index.html generado por angular dentro de templates de django

--- djangular/djangular/urls.py
+++ djangular/djangular/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# djangular/urls.py
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name="abg_home.html"), name="home")
+    path('', TemplateView.as_view(template_name="ang_home.html")),
 ]
