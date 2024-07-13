2717c1a87f0cd48e7a57a4b8122d60d91eedb753
Julio Chura <jchuraaca@unsa.edu.pe>
2024-07-13 15:23:11 -0500

    Se modifca api.service.ts

--- djangocrud/static/crud/src/app/api.service.spec.ts
+++ djangocrud/static/crud/src/app/api.service.spec.ts
@@ -1,16 +1,18 @@
-import { TestBed } from '@angular/core/testing';
-
-import { ApiService } from './api.service';
-
-describe('ApiService', () => {
-  let service: ApiService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from 
+'@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+ providedIn: 'root'
+})
+export class ApiService {
+ baseurl = "http://127.0.0.1:8000";
+ httpHeaders = new 
+ 
+HttpHeaders({'Content-Type':'application/json'});
+ constructor(private http:HttpClient) { }
+ getAllMovies():Observable<any>{
+ return this.http.get(this.baseurl+'/movie/', 
+ {headers: this.httpHeaders});
+ }
+}
