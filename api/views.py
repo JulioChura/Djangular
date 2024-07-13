8011c1df76c8b039d1377a31635f03d430d38118
Julio Chura <jchuraaca@unsa.edu.pe>
2024-07-13 15:02:41 -0500

    Se agrega la clase MovieViewSet en views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.models import User,Group
+from rest_framework import viewsets
+from .serializer import MovieSerializer
+from .models import Movie
+class MovieViewSet(viewsets.ModelViewSet):
+ queryset = Movie.objects.all()
+ serializer_class = MovieSerializer
